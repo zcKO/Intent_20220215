7ca761ee81a7ef085f0a04519f7e8d925db6dc94
JaeCheon <studyJcHub@gmail.com>
2022-02-15 16:51:04 +0900

    Intent 를 이용하여 화면 이동

--- app/src/main/java/com/jc/intent_20220215/MainActivity.kt
+++ app/src/main/java/com/jc/intent_20220215/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.jc.intent_20220215
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnMoveToOtherActivity.setOnClickListener {
+//            OtherActivity 로 이동
+            // 출발지 -> this : 현재 액티비티, 도착지 -> OtherActivity::class.java
+            val myIntent = Intent(this, OtherActivity::class.java)
+            startActivity(myIntent)
+        }
+
     }
 }
