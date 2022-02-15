419303f304ee8352df16374ceaacf811bb76ec4b
JaeCheon <studyJcHub@gmail.com>
2022-02-15 17:00:36 +0900

    finish() 를 이용해 메인으로 돌아가기

--- app/src/main/java/com/jc/intent_20220215/OtherActivity.kt
+++ app/src/main/java/com/jc/intent_20220215/OtherActivity.kt
@@ -1,11 +1,23 @@
 package com.jc.intent_20220215
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+        btnReturnToMain.setOnClickListener {
+
+//            val myIntent = Intent(this, MainActivity::class.java)
+//            startActivity(myIntent)
+
+//            돌아가는 경우에는 인텐트를 사용하지 않는다.
+            finish()
+
+        }
+
     }
 }
