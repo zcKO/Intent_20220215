83a898c58e784f4a7b92e6755a6e5532e7ffe918
JaeCheon <studyJcHub@gmail.com>
2022-02-16 10:16:40 +0900

    닉네임 변경 완료 버튼을 누르면, 데이터를 세팅해서 메인화면으로 복귀

--- app/src/main/java/com/jc/intent_20220215/EditNicknameActivity.kt
+++ app/src/main/java/com/jc/intent_20220215/EditNicknameActivity.kt
@@ -1,11 +1,33 @@
 package com.jc.intent_20220215
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nickname.*
 
 class EditNicknameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nickname)
+
+        btnSave.setOnClickListener {
+
+//            입력된 닉네임을 변수에 저장
+            val inputNickname = edtNickname.text.toString()
+
+//            복귀시 데이터를 들고 갈 Intent 를 생성.
+            val resultIntent = Intent()         // 결과를 담기 위한 용도로만 사용, (출발지 / 도착지) 명시 X
+
+//            Intent 에 결과 (데이터) 담기 -> putExtra 활용
+            resultIntent.putExtra("nick", inputNickname)
+
+//            실제 복귀 결과로 세팅
+//            확인을 눌렀다 + 들고 돌아갈 데이터 세팅 (2 가지 필요)
+            setResult(RESULT_OK, resultIntent)
+
+            finish()
+
+        }
+
     }
 }
